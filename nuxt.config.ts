b7c8c70b562dc86d6def27f954f38b6c58e075d4
senda0l <hairyfobbit@gmail.com>
2026-02-14 19:05:37 +0500

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "shortcut icon", href: "/favicon.svg" },
+      ],
+    },
+  },
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
   srcDir: "src/",
   css: ["~/assets/css/main.css"],
